1f968bc9aa9f00ebb27b767a2b39ad633ee71b32
george <george@georges-MacBook-Pro-2.local>
2017-09-14 13:42:03 +1000

    Initial Commit

---
+++ test drag 2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  test drag 2
+//
+//  Created by george on 9/14/17.
+//  Copyright © 2017 george. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ test drag 2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  test drag 2
+//
+//  Created by george on 9/14/17.
+//  Copyright © 2017 george. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
